bed93541d0a103468badfa16e26471c83193b906
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2020-03-28 15:41:15 -0400

    for loop review

--- arrays/todo.js
+++ arrays/todo.js
@@ -19,3 +19,9 @@ todos.forEach(function (todo, index) {
   const num = index + 1
   console.log(`${num}. ${todo}`)
 })
+
+// for (let count = 0; count < todos.length; count++) {
+//   const num = count + 1
+//   const todo = todos[count]
+//   console.log(`${num}. ${todo}.`)
+// }
