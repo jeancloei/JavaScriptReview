41aa629bd0cea57b6d64f0f4788a6e4972df0527
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2020-03-28 14:26:26 -0400

    Array intro review

---
+++ arrays/notes.js
@@ -0,0 +1,16 @@
+const notes = ['Note1', 'Note2', 'Note3']
+
+console.log(notes.length)
+console.log(notes[notes.length - 1])
+
+const todos = [
+  'Buy eggs', 
+  'Walk the dog', 
+  'Clean the house',
+  'Order cat food', 
+  'Study'  
+]
+
+console.log(`You have ${todos.length} todos!`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
